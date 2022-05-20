4dd67ed4bf41782131c08589f84281d5ae7433ff
unknown <dallasgood77@gmail.com>
2022-05-20 20:21:59 +0300

    init

---
+++ app/types.py
@@ -0,0 +1,1 @@
+from django.db.models import TextChoicesclass LanguageChoices(TextChoices):    ru = 'ru', 'Русский'    en = 'en', 'Английский'    uk = 'uk', 'Украинский'    be = 'be', 'Белорусский'    zn = 'zn', 'Китайский'
---
+++ app/urls.py
@@ -0,0 +1,1 @@
+from django.urls import pathfrom app.views import *urlpatterns = [    path('', IndexView.as_view(), name='index'),    path('resume/', ResumeView.as_view(), name='resume'),]
